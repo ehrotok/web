892e387903df721b44c77e1d8d3bf696a2ec606f
ikuosaito1989 <ikuosaito1989@gmail.com>
2025-01-07 21:53:35 +0900

    fix: addition of a constant

--- config/index.ts
+++ config/index.ts
@@ -2,10 +2,24 @@
  * ローカルストレージに保存するキー
  */
 const STORAGE_KEYS = {
-  BOOKMARK: "ehrotok_bookmark",
-  HISTORY: "ehrotok_history",
+  ACCOUNTS: "accounts",
+} as const;
+
+const COOKIE_KEYS = {
+  AGE_CHECK: "age_check",
+  BOOKMARK_ALERT: "bookmark_alert",
+} as const;
+
+const API_URLS = {
+  VIDEOS: "/v1/api/videos",
+  ACCOUNTS: "/v1/api/accounts",
+  BOOKMARKS: "/v1/api/accounts/bookmarks",
+  HISTORIES: "/v1/api/accounts/histories",
+  WATCH: "/v1/api/accounts/watch",
 } as const;
 
 export const Constants = {
   STORAGE_KEYS,
+  COOKIE_KEYS,
+  API_URLS,
 };
