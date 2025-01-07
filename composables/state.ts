958dfa84dfd302ed9425c8d99105de49a2495333
ikuosaito1989 <ikuosaito1989@gmail.com>
2025-01-07 21:54:13 +0900

    fix: add state

--- composables/state.ts
+++ composables/state.ts
@@ -2,17 +2,36 @@
  * ローディングを管理する状態
  * @returns
  */
-export const useLoader = () => useState<boolean>("loader", () => false);
+const useLoader = () => useState<boolean>("loader", () => false);
 
 /**
  * 横画面か
  * @returns
  */
-export const useLandscapeMode = () =>
-  useState<boolean>("landscapeMode", () => false);
+const useLandscapeMode = () => useState<boolean>("landscapeMode", () => false);
 
 /**
  * 選択した位置
  * @returns
  */
-export const usePositionState = () => useState<number>("position", () => 0);
+const usePositionState = () => useState<number>("position", () => 0);
+
+/**
+ * 年齢確認
+ * @returns
+ */
+const useAgeCheckState = () => useState<boolean>("age_check", () => true);
+
+/**
+ * トークン
+ * @returns
+ */
+const useTokenState = () => useState<string>("token", () => "");
+
+export {
+  useLoader,
+  useLandscapeMode,
+  usePositionState,
+  useAgeCheckState,
+  useTokenState,
+};
