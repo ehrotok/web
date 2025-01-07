e54256f10c26e8a471177ec70648f4282df3a0f5
ikuosaito1989 <ikuosaito1989@gmail.com>
2025-01-07 21:52:50 +0900

    refactor: import mdi/js all together

---
+++ config/mdijs.ts
@@ -0,0 +1,21 @@
+import {
+  mdiArrowULeftTop,
+  mdiHistory,
+  mdiBookmarkOutline,
+  mdiHomeOutline,
+  mdiHeart,
+  mdiBookmark,
+  mdiStar,
+  mdiAccountOutline,
+} from "@mdi/js";
+
+export {
+  mdiArrowULeftTop,
+  mdiHistory,
+  mdiBookmarkOutline,
+  mdiHomeOutline,
+  mdiHeart,
+  mdiBookmark,
+  mdiStar,
+  mdiAccountOutline,
+};
