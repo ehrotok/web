fb0c8eeb99b85f83275a5f78052792c650bc487f
ikuosaito1989 <ikuosaito1989@gmail.com>
2025-02-01 00:46:51 +0900

    fix: use thumbnail compone

--- pages/my-page.vue
+++ pages/my-page.vue
@@ -63,35 +63,7 @@
     </div>
   </div>
 
-  <div class="grid grid-cols-3">
-    <div
-      v-for="(tile, index) in tiles"
-      :key="index"
-      class="border-r border-b border-gray-500 [&:nth-child(3n)]:border-r-0"
-      @click="onClickTile(index)"
-    >
-      <div class="relative overflow-hidden shadow-md transition active:scale-150">
-        <nuxt-img
-          class="w-full h-40 object-contain"
-          :placeholder="Constants.IMAGES.BG_GRAY"
-          :src="tile.image"
-        />
-        <div
-          class="absolute bottom-0 left-0 w-full bg-gradient-to-b from-black/0 to-black/50 text-white p-2"
-        >
-          <p class="line-clamp-1 text-xs mt-1">{{ tile.title }}</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div
-    v-if="isLoading"
-    class="fixed bottom-5 left-1/2 transform -translate-x-1/2"
-  >
-    <div
-      class="animate-spin h-10 w-10 border-4 border-[#fe2c55] rounded-full border-t-transparent"
-    ></div>
-  </div>
+  <Thumbnails :tiles="tiles" v-model:isLoading="isLoading" @click="onClickTile"></Thumbnails>
 </template>
 
 <script setup lang="ts">
