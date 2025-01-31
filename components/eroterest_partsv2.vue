18de871d794d9856c8cae21c14a3c5c084f98416
ikuosaito1989 <ikuosaito1989@gmail.com>
2025-02-01 00:45:48 +0900

    feat: add eroterest component

---
+++ components/eroterest_partsv2.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div id="eroterest_partsv2" class="eroterest_partsv2"></div>
+</template>
+
+<script setup lang="ts">
+onMounted(() => {
+  const targetDiv = document.getElementById('eroterest_partsv2')
+  if (targetDiv) {
+    const script = document.createElement('script')
+    script.src =
+      '//bpm.eroterest.net/v2/?w=&e=&n=9&rn=3&spn=&sprn=&fc=&ts=0&tp=0&tbg=&tsz=&tr=&tc=&tmc=&tu=1&is=1&mts=&ds=&dsz=&dr=&dc=&df=&ibg=&ibm=&ids=&ib=&ibc=&ir=&sf='
+    script.async = true
+
+    targetDiv.appendChild(script) // `#eroterest_partsv2` の中に追加
+  }
+})
+</script>
