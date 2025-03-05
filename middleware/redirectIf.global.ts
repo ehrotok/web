59f8c3deb8666a31227c41b9fb5aee6d00d36b7d
ikuosaito1989 <ikuosaito1989@gmail.com>
2025-03-05 10:25:32 +0900

    fix: Redirect to sitemap of another domain

--- middleware/redirectIf.global.ts
+++ middleware/redirectIf.global.ts
@@ -1,8 +1,11 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const config = useRuntimeConfig()
-
   if (config.public.isNetlify) {
     return navigateTo('https://ehrotok.com/', { external: true })
+  }
+
+  if (to.fullPath === '/sitemap.xml' || to.fullPath === '/sitemap_index.xml') {
+    return navigateTo('https://ja.ehrotok.com/sitemap.xml', { external: true })
   }
 
   return
